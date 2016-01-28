0bf3732e468020b4c3dd8fdfc6cac055c0646dd2
web-padawan <skamandr@yandex.ru>
2016-01-28 13:42:20 +0200

    [new] usage block

--- themes/littlevit/layouts/front/front-layout.tpl.php
+++ themes/littlevit/layouts/front/front-layout.tpl.php
@@ -70,3 +70,98 @@
       </div>
     </div>
   </div>
+
+  <div class="usage">
+    <div class="usage__inner">
+      <h2 class="usage__title">Спосіб вживання</h2>
+
+       <ul class="usage__list">
+         <li class="usage__item">
+           <div class="usage__age">
+              <div class="usage__icon usage__icon--baby">&nbsp;</div>
+              <div class="usage__descr">дітям віком</div>
+              <div class="usage__value">4-6 місяців</div>
+           </div>
+           <div class="usage__dose">0,25 мл на добу</div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__age">
+              <div class="usage__icon usage__icon--baby">&nbsp;</div>
+              <div class="usage__descr">дітям віком</div>
+              <div class="usage__value">7-12 місяців</div>
+           </div>
+           <div class="usage__dose">0,5 мл на добу</div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__age">
+              <div class="usage__icon usage__icon--child">&nbsp;</div>
+              <div class="usage__descr">дітям віком</div>
+              <div class="usage__value">12-36 місяців</div>
+           </div>
+           <div class="usage__dose">1 мл на добу</div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__age">
+              <div class="usage__icon usage__icon--child">&nbsp;</div>
+              <div class="usage__descr">дітям віком</div>
+              <div class="usage__value">3-7 років</div>
+           </div>
+           <div class="usage__dose">1,5 мл на добу</div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__age">
+              <div class="usage__icon usage__icon--child">&nbsp;</div>
+              <div class="usage__descr">дітям віком</div>
+              <div class="usage__value">старше 7-ми років</div>
+           </div>
+           <div class="usage__dose">2 мл на добу</div>
+         </li>
+       </ul>
+
+       <ul class="usage__list">
+         <li class="usage__item">
+           <div class="usage__step">
+              <div class="usage__number">
+                <span class="usage__digit">1</span>
+              </div>
+              <div class="usage__guide">Добре збовтати пляшечку перед використанням.</div>
+           </div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__step">
+              <div class="usage__number">
+                <span class="usage__digit">2</span>
+              </div>
+              <div class="usage__guide">
+                <span>Наповнити рідиною піпетку до необхідного рівня та ввести напряму або</span>
+                <span>додати до води, соку або дитячого харчування.</span>
+              </div>
+           </div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__step">
+              <div class="usage__number">
+                <span class="usage__digit">3</span>
+              </div>
+             <div class="usage__guide">Прополоскати піпетку після використання.</div>
+           </div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__step">
+              <div class="usage__number">
+                <span class="usage__digit">4</span>
+              </div>
+             <div class="usage__guide">Не перевищувати рекомендовану дозу.</div>
+           </div>
+         </li>
+         <li class="usage__item">
+           <div class="usage__step">
+              <div class="usage__number">
+                <span class="usage__digit">5</span>
+              </div>
+             <div class="usage__guide">Використовувати за рекомендацією лікаря як додаткове джерело вітамінів.</div>
+           </div>
+         </li>
+       </ul>
+    </div>
+  </div>
